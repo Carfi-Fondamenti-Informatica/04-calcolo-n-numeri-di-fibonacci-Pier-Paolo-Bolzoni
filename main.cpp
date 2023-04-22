a645154ce3156c50c230a48310dd72874af4eae9
Pier-Paolo-Bolzoni <100352364+Pier-Paolo-Bolzoni@users.noreply.github.com>
2023-04-22 15:42:00 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,23 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-   return 0;
+int main(){
+
+    int n, x1=1, x2=0, x3;
+
+    cin >> n;
+
+    if(n>=2){
+        for(int i=1; i<=n; i++){
+
+            x3 = x1 + x2;
+            cout << x3 << endl;
+            x1 = x2;
+            x2 = x3;
+        }
+    }else
+        cout << "errore";
+
+
+    return 0;
 }
